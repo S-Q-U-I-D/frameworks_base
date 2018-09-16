714ef74c2a71fc3bf8b29f6b34ff8c9fda06df67
varund7726 <date.varun123@gmail.com>
2018-09-16 16:44:16 +0530

    Option to Display Data Disabled Indicator Icon [1/2]
    Change-Id: Ib14eb047715d93667774dfa699ea1b97a9c2ab53

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4293,6 +4293,12 @@ public final class Settings {
          */
         public static final String FULL_GESTURE_NAVBAR_DT2S = "full_gesture_navbar_dt2s";
 
+	     /**
+         * Whether to display cross sign for a data disabled connection
+         * @hide
+         */
+        public static final String DATA_DISABLED_ICON = "data_disabled_icon";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
