d783131f817cef3723c27b561270756c0e4c5edc
Yingren Wang <yingrenw@codeaurora.org>
2018-12-25 13:47:42 +0800

    SystemUI: Fix an issue where jumping to black after screen turned on
    When press the power button several times, the screen will probabilistically jump from
    the keyguard to the black screen after screen turned on. at this scene, previoud scrim
    AOD state change triggered screen turned off is not performed until screen turned on.
    Front ScrimView will display black color and covor keyguard in AOD state. so we can see
    keyguard and then jump to black screen after screen turned on.
    
    Ignore scrim AOD state change after screen turned on.
    
    Change-Id: I7b206f0599420d2790b49f409a64fc264a4ecca0
    CRs-Fixed: 2342473
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4915,7 +4915,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void startDozing() {
-            if (!mDozingRequested) {
+             if (!mDozingRequested && !isScreenTurningOnOrOn()) {
                 mDozingRequested = true;
                 DozeLog.traceDozing(mContext, mDozing);
                 updateDozing();
@@ -4964,7 +4964,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                 DozeLog.traceDozing(mContext, mDozing);
                 mWakefulnessLifecycle.dispatchStartedWakingUp();
                 updateDozing();
-            }
+
+                updateScrimController();
+           }
         }
 
         @Override
