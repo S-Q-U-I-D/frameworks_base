juvevood/android_frameworks_base

Battery saver: Ensure warning doesn't pop up while plugged in
This doesn't make any sense because if you're plugged in then you don't
need the battery saver.

- Introduced method to determine if plugged in or not, works with wireless
charging as well.

Steps to duplicate fail:
- Clean flash or make sure you've never turned on battery saver
- Plug in your phone
- Pull down QS panel
- Tap on the battery QS tile

Change-Id: Idd9f5382ac68b5af0cf8e3d65c670c2982b62a54

packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
@@ -690,7 +690,9 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
                 dismissLowBatteryNotification();
             } else if (action.equals(ACTION_SHOW_START_SAVER_CONFIRMATION)) {
                 dismissLowBatteryNotification();
-                showStartSaverConfirmation();
+                if (!com.android.internal.util.du.Utils.isPlugged(context)) {
+                    showStartSaverConfirmation();
+                }
             } else if (action.equals(ACTION_DISMISSED_WARNING)) {
                 dismissLowBatteryWarning();
             } else if (ACTION_CLICKED_TEMP_WARNING.equals(action)) {

S-Q-U-I-D/frameworks_base

Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
@@ -659,7 +659,9 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
                 dismissLowBatteryNotification();
             } else if (action.equals(ACTION_SHOW_START_SAVER_CONFIRMATION)) {
                 dismissLowBatteryNotification();
-                showStartSaverConfirmation();
+                if (!com.android.internal.util.aosp.AospUtils.isPlugged(context)) {
+                    showStartSaverConfirmation();
+                }
             } else if (action.equals(ACTION_DISMISSED_WARNING)) {
                 dismissLowBatteryWarning();
             } else if (ACTION_CLICKED_TEMP_WARNING.equals(action)) {